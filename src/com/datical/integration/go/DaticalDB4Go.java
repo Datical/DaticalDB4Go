cd8238e238fe4fbb5839e182dcb77248cb9ee28d
r2datical <r2@datical.com>
2015-07-14 13:30:18 -0500

    More project setup

---
+++ src/com/datical/integration/go/DaticalDB4Go.java
@@ -0,0 +1,11 @@
+package com.datical.integration.go;
+
+import com.thoughtworks.go.plugin.api.annotation.Extension;
+import com.thoughtworks.go.plugin.api.info.PluginDescriptor;
+import com.thoughtworks.go.plugin.api.info.PluginDescriptorAware;
+
+@Extension
+public class DaticalDB4Go implements PluginDescriptorAware {
+	public void setPluginDescriptor(PluginDescriptor descriptor) {
+	}
+}
