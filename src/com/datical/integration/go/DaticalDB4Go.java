410f01d14f58ccf8881b7a5aacfd64c026988f04
r2datical <r2@datical.com>
2015-07-30 11:18:50 -0500

    Putting a little work...

--- src/com/datical/integration/go/DaticalDB4Go.java
+++ src/com/datical/integration/go/DaticalDB4Go.java
@@ -1,11 +1,30 @@
 package com.datical.integration.go;
 
 import com.thoughtworks.go.plugin.api.annotation.Extension;
+import com.thoughtworks.go.plugin.api.annotation.Load;
+import com.thoughtworks.go.plugin.api.annotation.UnLoad;
+import com.thoughtworks.go.plugin.api.info.PluginContext;
 import com.thoughtworks.go.plugin.api.info.PluginDescriptor;
 import com.thoughtworks.go.plugin.api.info.PluginDescriptorAware;
 
+import com.thoughtworks.go.plugin.api.logging.Logger;
+
 @Extension
 public class DaticalDB4Go implements PluginDescriptorAware {
+	
+	Logger logger;
+	
 	public void setPluginDescriptor(PluginDescriptor descriptor) {
+	}
+
+	@Load
+	public void onLoad(PluginContext context) {
+		logger.info("Plugin loaded.");
+	}
+
+	@UnLoad
+	public void onUnload(PluginContext context) {
+		logger.info("Plugin unloaded.");
+		
 	}
 }
